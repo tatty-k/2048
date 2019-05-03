85824799759e546cd0b249f9a8c2f30111c97390
Tatty <tattyklass@gmail.com>
2019-05-03 11:46:53 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*----- constants -----*/ 
+const 
+
+/*----- app's state (variables) -----*/ 
+
+
+
+/*----- cached element references -----*/ 
+
+
+
+/*----- event listeners -----*/ 
+
+
+
+
+/*----- functions -----*/
+
+function init(){
+    board = [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+    ];
+}
